e875dec60d4e014514f3f0ccc485366b4271ab98
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-03-19 13:43:15 +0200

    Update Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,85 +1,85 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import { Link } from 'react-router-dom';
+import { Typography, Badge, Drawer, List, ListItem } from '@mui/material';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = (isOpen: boolean) => (event: React.MouseEvent) => {
+    setIsOpen(isOpen);
+  };
+
   return (
-    <header className="px-3 sm:px-8 sm:text-lg font-medium">
-      <div className="py-4 flex justify-between items-center">
-        <div className="flex gap-3 item-center">
-          <div className="flex items-center">
-            <img className="inline-block" src="./img/en.png" alt="language icon" />
-            <KeyboardArrowDownIcon />
-          </div>
-          <div className="flex items-center">
-            <span className="uppercase">usd</span>
-            <KeyboardArrowDownIcon />
-          </div>
-          <nav>
-            <ul className="flex gap-3 items-end">
-              <li>
-                <Link className="border-black hover:border-b-2" to="/products/1">
-                  Women
+    <header className="grid gap-2 p-4 max-w-[1100px] m-auto">
+      <div className="flex justify-between items-center border-b-2 pb-3">
+        <nav className="flex-1">
+          <button onClick={toggleDrawer(true)}>
+            <MenuIcon sx={{ fontSize: '2rem' }} />
+          </button>
+          <Drawer anchor="left" open={isOpen} onClose={toggleDrawer(false)}>
+            <List sx={{ width: 'min(300px, 60vw)' }}>
+              <ListItem>
+                <Link className="hover:text-black" to="/">
+                  Home
                 </Link>
-              </li>
-              <li>
-                <Link className="border-black hover:border-b-2" to="/products/2">
-                  Men
+              </ListItem>
+              <ListItem>
+                <Link className="hover:text-black" to="/">
+                  About
                 </Link>
-              </li>
-              <li>
-                <Link className="border-black hover:border-b-2" to="/products/3">
-                  Children
+              </ListItem>
+              <ListItem>
+                <Link className="hover:text-black" to="/">
+                  Contact
                 </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div>
+              </ListItem>
+              <ListItem>
+                <Link className="hover:text-black" to="/">
+                  Store
+                </Link>
+              </ListItem>
+            </List>
+          </Drawer>
+        </nav>
+        <Typography
+          component="h1"
+          sx={{ display: { xs: 'none', sm: 'block' }, flex: 1, textAlign: 'center' }}>
           <Link to="/" className="text-5xl">
             Rêveur
           </Link>
-        </div>
-        <div className="flex gap-8 items-end">
-          <nav>
-            <ul className="flex gap-3">
-              <li>
-                <Link className="border-black hover:border-b-2" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="border-black hover:border-b-2" to="/">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link className="border-black hover:border-b-2" to="/">
-                  Contact
-                </Link>
-              </li>
-              <li>
-                <Link className="border-black hover:border-b-2" to="/">
-                  Stores
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <div className="flex gap-2 items-end text-gray-600 cursor-pointer">
-            <SearchIcon className="hover:text-gray-900" />
-            <PersonIcon className="hover:text-gray-900" />
-            <FavoriteBorderOutlinedIcon className="hover:text-gray-900" />
-            <span className="relative">
-              <ShoppingCartOutlinedIcon className="hover:text-gray-900" />
-              <span className="text-sm w-5 h-5 rounded-2xl bg-blue-500 text-white absolute -top-1 -right-2 flex items-center justify-center">
-                0
-              </span>
-            </span>
-          </div>
-        </div>
+        </Typography>
+        <menu className="flex flex-1 justify-end gap-2">
+          <Link to="/">
+            <SearchIcon className="cursor-pointer text-gray-600 hover:text-gray-800 transition-colors" />
+          </Link>
+          <Link to="/">
+            <PersonIcon className="cursor-pointer text-gray-600 hover:text-gray-800 transition-colors" />
+          </Link>
+          <Link to="/">
+            <FavoriteBorderOutlinedIcon className="cursor-pointer text-gray-600 hover:text-gray-800 transition-colors" />
+          </Link>
+          <Link to="/">
+            <Badge badgeContent={'0'} color="primary">
+              <ShoppingCartOutlinedIcon className="cursor-pointer text-gray-600 hover:text-gray-800 transition-colors" />
+            </Badge>
+          </Link>
+        </menu>
+      </div>
+      <div className="flex justify-center gap-3 sm:text-xl">
+        <Link className="border-black hover:border-b-2" to="/products/1">
+          Women
+        </Link>
+        <Link className="border-black hover:border-b-2" to="/products/2">
+          Men
+        </Link>
+        <Link className="border-black hover:border-b-2" to="/products/3">
+          Children
+        </Link>
       </div>
     </header>
   );
