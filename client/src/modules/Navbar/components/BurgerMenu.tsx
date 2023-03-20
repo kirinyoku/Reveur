7ac3b1850601d85e66e9be4ef3da5e51bb819a85
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-03-20 19:13:24 +0200

    Update BurgerMenu

--- client/src/modules/Navbar/components/BurgerMenu.tsx
+++ client/src/modules/Navbar/components/BurgerMenu.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Drawer, List, ListItem } from '@mui/material';
+import { SwipeableDrawer, List, ListItem } from '@mui/material';
 import { BurgerMenuProps } from '../types/BurgerMenu';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const BurgerMenu = ({ anchor, pages, className }: BurgerMenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,11 +17,20 @@ const BurgerMenu = ({ anchor, pages, className }: BurgerMenuProps) => {
       <button onClick={toggleDrawer(true)}>
         <MenuIcon sx={{ fontSize: '2rem' }} />
       </button>
-      <Drawer anchor={anchor} open={isOpen} onClose={toggleDrawer(false)}>
-        <List sx={{ width: 'min(25rem, 60vw)', fontSize: '1.3rem' }}>
+      <SwipeableDrawer
+        anchor={anchor}
+        open={isOpen}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}>
+        <button onClick={toggleDrawer(false)} className="flex justify-end px-2 pt-2">
+          <CloseIcon sx={{ fontSize: '2rem' }} />
+        </button>
+        <List sx={{ width: 'min(25rem, 100vw)', fontSize: '1.3rem', padding: '0' }}>
           {pages.map((page) => {
             return (
-              <ListItem key={page.title}>
+              <ListItem
+                key={page.title}
+                sx={{ borderBottom: '1px solid #c8ccd5', paddingTop: '0', marginBottom: '8px' }}>
                 <Link className="hover:text-black" to={page.to}>
                   {page.title}
                 </Link>
@@ -28,7 +38,7 @@ const BurgerMenu = ({ anchor, pages, className }: BurgerMenuProps) => {
             );
           })}
         </List>
-      </Drawer>
+      </SwipeableDrawer>
     </menu>
   );
 };
