3ff994ad454af9da0a11711fa49ff6d83381455c
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-03-22 19:38:55 +0200

    Update SectionProductsProps

--- client/src/modules/SectionProducts/types/SectionProducts.ts
+++ client/src/modules/SectionProducts/types/SectionProducts.ts
@@ -1,4 +1,12 @@
-import { card } from '../../../types/card';
+type card = {
+  id: number;
+  img: string;
+  img2: string;
+  title: string;
+  oldPrice?: number;
+  currentPrice: number;
+  isNew?: boolean;
+};
 
 export type SectionProductsProps = {
   type: string;
