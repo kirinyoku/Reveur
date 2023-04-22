838a4b6d0588ad9c8b3af891fae1c837f5b37904
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-04-22 15:23:20 +0300

    Add payment status pages

---
+++ app/(payment)/cancel/page.tsx
@@ -0,0 +1,8 @@
+export default function Cancel() {
+  return (
+    <main className="flex flex-col gap-2 justify-center items-center h-screen">
+      <h2 className="text-3xl uppercase">Order canceled</h2>
+      <p className="text-center text-xl">Sorry, your order has been canceled.</p>
+    </main>
+  );
+}
---
+++ app/(payment)/success/page.tsx
@@ -0,0 +1,7 @@
+export default function Success() {
+  return (
+    <main className="flex flex-col gap-2 justify-center items-center h-screen">
+      <h2 className="text-3xl uppercase">Thank you for your order</h2>
+    </main>
+  );
+}
